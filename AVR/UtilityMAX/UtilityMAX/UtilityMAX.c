687378ba66e526a8b9ebbe4e9770a74932c90561
me.vieda372 <me.vieda372@dd178765-cc16-abd5-2fbf-13a62dcdf5e5>
2011-04-27 01:43:21 +0000

    Creación del proyecto general. Se agrega la libreria para el control de la USART del ATmega168 desarrollado

---
+++ AVR/UtilityMAX/UtilityMAX/UtilityMAX.c
@@ -0,0 +1,45 @@
+/*
+ * UNIVERSIDAD DE LOS ANDES - FACULTAD DE INGENIERIA
+ * DEPARTAMENTO DE INGENIERIA DE SISTEMAS Y COMPUTACIÓN
+ *
+ * FILE:	 UtilityMAX.c
+ * CREATED:	 26/04/2011
+ * MODIFIED: 26/04/2011
+ * AUTHOR:	 Manuel Vieda <http://manuelvieda.com>
+ */ 
+
+#define F_CPU 20000000UL
+
+
+#include <stdio.h>			// Standard IO facilities 
+#include <stdint.h>			// Standard Integer Types
+#include <avr/io.h>			// AVR device-specific IO definitions
+#include <avr/interrupt.h>	// Interrupts handling
+#include <util/delay.h>		// Convenience functions for busy-wait delay loops
+
+
+
+//  ---------------------------------------------------------------------------
+//     GLOBAL SYSTEM VARIABLES / BUFFERS
+//  ---------------------------------------------------------------------------
+
+//  ---------------------------------------------------------------------------
+//      DECLARATION OF METHODS / INTERNAL PROCEDURES
+//  ---------------------------------------------------------------------------
+
+
+//  ---------------------------------------------------------------------------
+//      MAIN PROGRAM / ROUTINE
+//  ---------------------------------------------------------------------------
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
+
+//  ---------------------------------------------------------------------------
+//      IMPLEMENTATION OF METHODS / ROUTINES / PROCEDURES
+//  ---------------------------------------------------------------------------
