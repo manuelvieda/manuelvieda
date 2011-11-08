39c564f349020cf71d4f3969bbf3df18ac285cf2
me.vieda372@gmail.com <me.vieda372@gmail.com@dd178765-cc16-abd5-2fbf-13a62dcdf5e5>
2011-11-08 12:37:10 +0000

    Inicio de cliente para servicios EC2 de Amazon. Se finaliza creacion de instancia y se una placeGroup.

---
+++ trunk/UnaCloud/UnaCloudEJB/src/main/java/com/manuelvieda/unacloud/repository/constants/AWSConstants.java
@@ -0,0 +1,22 @@
+/**
+ * UnaCloud - High Performance Computing on the Cloud
+ * Manuel Eduardo Vieda Salomón (mail@manuelvieda.com)
+ *
+ * Creation: 07/11/2011
+ * Project: UnaCloudEJB
+ * Package: com.manuelvieda.unacloud.repository.constants
+ * File:    AWSConstants.java
+ */
+package com.manuelvieda.unacloud.repository.constants;
+
+/**
+ *
+ * @author Manuel Eduardo Vieda Salomon  (mail@manuelvieda.com)
+ * @version	1.0
+ * @since	1.0
+ */
+public class AWSConstants {
+
+	public final static String EC2_PLACEMENT_GROUP_CLUSTER_STRATEGY = "cluster";
+	
+}
