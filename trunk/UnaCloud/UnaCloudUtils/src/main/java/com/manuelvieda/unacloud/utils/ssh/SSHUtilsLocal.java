ab22c0de0af2b505b73b204d639e37b03ca2e0ca
me.vieda372@gmail.com <me.vieda372@gmail.com@dd178765-cc16-abd5-2fbf-13a62dcdf5e5>
2011-11-15 12:23:33 +0000

    Utilidades para conexion y comuniacion SSH

---
+++ trunk/UnaCloud/UnaCloudUtils/src/main/java/com/manuelvieda/unacloud/utils/ssh/SSHUtilsLocal.java
@@ -0,0 +1,19 @@
+/**
+ * UnaCloud - High Performance Computing on the Cloud
+ * Manuel Eduardo Vieda Salomón (mail@manuelvieda.com)
+ *
+ * Creation: 23/10/2011
+ * Project: UnaCloudUtils
+ * Package: com.manuelvieda.unacloud.utils.ssh
+ * File:    SSHUtilsLocal.java
+ */
+package com.manuelvieda.unacloud.utils.ssh;
+import javax.ejb.Local;
+
+import net.schmizz.sshj.SSHClient;
+
+@Local
+public interface SSHUtilsLocal {
+	
+
+}
