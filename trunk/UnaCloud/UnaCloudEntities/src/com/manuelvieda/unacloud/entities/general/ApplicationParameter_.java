2f33233cc7a148b94cb879cee9b032e669178de8
me.vieda372@gmail.com <me.vieda372@gmail.com@dd178765-cc16-abd5-2fbf-13a62dcdf5e5>
2011-11-28 13:39:54 +0000

    Cambio en Entities

--- trunk/UnaCloud/UnaCloudEntities/src/com/manuelvieda/unacloud/entities/general/ApplicationParameter_.java
+++ trunk/UnaCloud/UnaCloudEntities/src/com/manuelvieda/unacloud/entities/general/ApplicationParameter_.java
@@ -4,12 +4,12 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2011-11-15T04:16:35.325-0500")
+@Generated(value="Dali", date="2011-11-27T20:50:58.076-0500")
 @StaticMetamodel(ApplicationParameter.class)
 public class ApplicationParameter_ {
 	public static volatile SingularAttribute<ApplicationParameter, Integer> id;
 	public static volatile SingularAttribute<ApplicationParameter, String> description;
-	public static volatile SingularAttribute<ApplicationParameter, Byte> mandatory;
+	public static volatile SingularAttribute<ApplicationParameter, Boolean> mandatory;
 	public static volatile SingularAttribute<ApplicationParameter, String> name;
 	public static volatile SingularAttribute<ApplicationParameter, String> type;
 	public static volatile SingularAttribute<ApplicationParameter, Application> application;
